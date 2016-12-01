9b334a8e66f246c604d5e74721d2811b7600c784
dong <gxd0826@126.com>
2016-12-01 16:06:53 +0800

    初始化操作

---
+++ lianna/lianna/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lianna
+//
+//  Created by 郭旭东 on 16/12/1.
+//  Copyright © 2016年 jackguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lianna/lianna/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lianna
+//
+//  Created by 郭旭东 on 16/12/1.
+//  Copyright © 2016年 jackguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
